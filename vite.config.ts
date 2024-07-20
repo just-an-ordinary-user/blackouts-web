40e154664682c97ebb08e47bdc0bbc0623de6097
just-an-ordinary-user <evgenmoruzhenko@gmail.com>
2024-07-20 20:45:56 +0300

    Introduced PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -7,7 +9,41 @@ export default defineConfig(({ mode }) => {
   const baseUrl = `${env.VITE_BASE_URL ?? "/"}`;
 
   return {
-    plugins: [react()],
+    plugins: [
+      mkcert(),
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          theme_color: "#000000",
+          icons: [
+            {
+              src: `${baseUrl}icon-192x192.png`,
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: `${baseUrl}icon-256x256.png`,
+              sizes: "256x256",
+              type: "image/png",
+            },
+            {
+              src: `${baseUrl}icon-384x384.png`,
+              sizes: "384x384",
+              type: "image/png",
+            },
+            {
+              src: `${baseUrl}icon-512x512.png`,
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
     base: baseUrl,
     server: {
       host: true,
