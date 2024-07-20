81d84d60572310583ae794e1586ca1a232f2658e
just-an-ordinary-user <evgenmoruzhenko@gmail.com>
2024-07-20 16:03:48 +0300

    Add env and baseUrl support. Set baseUrl env variable to workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const baseUrl = `${env.VITE_BASE_URL ?? "/"}`;
+
+  return {
+    plugins: [react()],
+    base: baseUrl,
+    server: {
+      host: true,
+    },
+  };
 });
