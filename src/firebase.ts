924aa6ca09e355e75945bee65b758b972d36fe16
just-an-ordinary-user <evgenmoruzhenko@gmail.com>
2024-11-30 19:46:03 +0200

    Migrating to firebase production mode.
    Moved firebase settings to env file.

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,8 +3,8 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
